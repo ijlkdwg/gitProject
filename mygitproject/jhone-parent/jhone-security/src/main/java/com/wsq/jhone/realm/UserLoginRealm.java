35a0f95156d613031559b512baea9218795c693b
ijlkdwg <wen123103624@126.com>
2016-06-01 17:32:57 +0800

    丰富了很多框架的配置

--- mygitproject/jhone-parent/jhone-security/src/main/java/com/wsq/jhone/realm/UserLoginRealm.java
+++ mygitproject/jhone-parent/jhone-security/src/main/java/com/wsq/jhone/realm/UserLoginRealm.java
@@ -1,5 +1,24 @@
 package com.wsq.jhone.realm;
 
-public class UserLoginRealm {
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+public class UserLoginRealm extends AuthorizingRealm{
+
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection arg0) {
+		
+		return null;
+	}
+
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken arg0) throws AuthenticationException {
+
+		return null;
+	}
 
 }
