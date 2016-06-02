92d28943678ee4dbabb31576943c12f184da1054
ijlkdwg <wen123103624@126.com>
2016-06-02 14:12:42 +0800

    提交一版

--- mygitproject/jhone-parent/jhone-security/src/main/java/com/wsq/jhone/realm/UserLoginRealm.java
+++ mygitproject/jhone-parent/jhone-security/src/main/java/com/wsq/jhone/realm/UserLoginRealm.java
@@ -1,5 +1,7 @@
 package com.wsq.jhone.realm;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -8,16 +10,16 @@ import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 
 public class UserLoginRealm extends AuthorizingRealm{
-
+	private final Logger logger = LoggerFactory.getLogger(UserLoginRealm.class);
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection arg0) {
-		
+		logger.info("进入判断");
 		return null;
 	}
 
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken arg0) throws AuthenticationException {
-
+		logger.info("进入判断1");
 		return null;
 	}
 
